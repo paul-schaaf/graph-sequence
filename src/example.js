6f2ab394562cc495189a8c935521539fe3c75cb0
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-07-09 10:20:21 +0200

    added values to leafs in example scenario

--- src/example.js
+++ src/example.js
@@ -1,60 +1,3 @@
-/*
-export default [
-  'digraph {\n'
-    + '\tnode [shape=record];\n'
-    + '\tstruct0 [label="<f0> |<f1> 3|<f2> "];\n'
-    + '\tstruct0:<f0> -> struct1;\n'
-    + '\tstruct1 [label="<f0> |<f1> 2|<f2> "];\n'
-    + '\tstruct1:<f0> -> struct2;\n'
-    + '\tstruct2 [label=leaf, shape=ellipse];\n'
-    + '\tstruct1:<f2> -> struct3;\n'
-    + '\tstruct3 [label=leaf, shape=ellipse];\n'
-    + '\tstruct0:<f2> -> struct4;\n'
-    + '\tstruct4 [label="<f0> |<f1> 4|<f2> "];\n'
-    + '\tstruct4:<f0> -> struct5;\n'
-    + '\tstruct5 [label=leaf, shape=ellipse];\n'
-    + '\tstruct4:<f2> -> struct6;\n'
-    + '\tstruct6 [label=leaf, shape=ellipse];\n'
-    + '}',
-  'digraph {\n'
-  + '\tnode [shape=record];\n'
-  + '\tstruct0 [label="<f0> |<f1> 3|<f2> "];\n'
-  + '\tstruct0:<f0> -> struct1;\n'
-  + '\tstruct1 [label=""];\n'
-  + '\tstruct1:<f2> -> struct3;\n'
-  + '\tstruct3 [label=leaf, shape=ellipse];\n'
-  + '\tstruct0:<f2> -> struct4;\n'
-  + '\tstruct4 [label="<f0> |<f1> 4|<f2> "];\n'
-  + '\tstruct4:<f0> -> struct5;\n'
-  + '\tstruct5 [label=leaf, shape=ellipse];\n'
-  + '\tstruct4:<f2> -> struct6;\n'
-  + '\tstruct6 [label=leaf, shape=ellipse];\n'
-  + '}',
-  'digraph {\n'
-  + '\tnode [shape=record];\n'
-  + '\tstruct0 [label=""];\n'
-  + '\tstruct3 [label=leaf, shape=ellipse];\n'
-  + '\tstruct0 -> struct4;\n'
-  + '\tstruct4 [label="<f-2> |<f-1> 3|<f0> |<f1> 4|<f2> "];\n'
-  + '\tstruct4:<f0> -> struct5;\n'
-  + '\tstruct4:<f-2> -> struct3;\n'
-  + '\tstruct5 [label=leaf, shape=ellipse];\n'
-  + '\tstruct4:<f2> -> struct6;\n'
-  + '\tstruct6 [label=leaf, shape=ellipse];\n'
-  + '}',
-  'digraph {\n'
-  + '\tnode [shape=record];\n'
-  + '\tstruct3 [label=leaf, shape=ellipse];\n'
-  + '\tstruct4 [label="<f-2> |<f-1> 3|<f0> |<f1> 4|<f2> "];\n'
-  + '\tstruct4:<f0> -> struct5;\n'
-  + '\tstruct4:<f-2> -> struct3;\n'
-  + '\tstruct5 [label=leaf, shape=ellipse];\n'
-  + '\tstruct4:<f2> -> struct6;\n'
-  + '\tstruct6 [label=leaf, shape=ellipse];\n'
-  + '}',
-];
-*/
-
 export default {
   name: 'abtree-merge',
   frames: [
@@ -66,15 +9,15 @@ export default {
           + '\tstruct0:<f0> -> struct1;\n'
           + '\tstruct1 [label="<f0> |<f1> 2|<f2> "];\n'
           + '\tstruct1:<f0> -> struct2;\n'
-          + '\tstruct2 [label=leaf, shape=ellipse];\n'
+          + '\tstruct2 [label=2, shape=ellipse];\n'
           + '\tstruct1:<f2> -> struct3;\n'
-          + '\tstruct3 [label=leaf, shape=ellipse];\n'
+          + '\tstruct3 [label=3, shape=ellipse];\n'
           + '\tstruct0:<f2> -> struct4;\n'
           + '\tstruct4 [label="<f0> |<f1> 4|<f2> "];\n'
           + '\tstruct4:<f0> -> struct5;\n'
-          + '\tstruct5 [label=leaf, shape=ellipse];\n'
+          + '\tstruct5 [label=4, shape=ellipse];\n'
           + '\tstruct4:<f2> -> struct6;\n'
-          + '\tstruct6 [label=leaf, shape=ellipse];\n'
+          + '\tstruct6 [label=dummy, shape=ellipse];\n'
           + '}',
     },
     {
@@ -85,13 +28,13 @@ export default {
           + '\tstruct0:<f0> -> struct1;\n'
           + '\tstruct1 [label=""];\n'
           + '\tstruct1:<f2> -> struct3;\n'
-          + '\tstruct3 [label=leaf, shape=ellipse];\n'
+          + '\tstruct3 [label=3, shape=ellipse];\n'
           + '\tstruct0:<f2> -> struct4;\n'
           + '\tstruct4 [label="<f0> |<f1> 4|<f2> "];\n'
           + '\tstruct4:<f0> -> struct5;\n'
-          + '\tstruct5 [label=leaf, shape=ellipse];\n'
+          + '\tstruct5 [label=4, shape=ellipse];\n'
           + '\tstruct4:<f2> -> struct6;\n'
-          + '\tstruct6 [label=leaf, shape=ellipse];\n'
+          + '\tstruct6 [label=dummy, shape=ellipse];\n'
           + '}',
     },
     {
@@ -99,27 +42,27 @@ export default {
       graph: 'digraph {\n'
           + '\tnode [shape=record];\n'
           + '\tstruct0 [label=""];\n'
-          + '\tstruct3 [label=leaf, shape=ellipse];\n'
+          + '\tstruct3 [label=3, shape=ellipse];\n'
           + '\tstruct0 -> struct4;\n'
           + '\tstruct4 [label="<f-2> |<f-1> 3|<f0> |<f1> 4|<f2> "];\n'
           + '\tstruct4:<f0> -> struct5;\n'
           + '\tstruct4:<f-2> -> struct3;\n'
-          + '\tstruct5 [label=leaf, shape=ellipse];\n'
+          + '\tstruct5 [label=4, shape=ellipse];\n'
           + '\tstruct4:<f2> -> struct6;\n'
-          + '\tstruct6 [label=leaf, shape=ellipse];\n'
+          + '\tstruct6 [label=dummy, shape=ellipse];\n'
           + '}',
     },
     {
       name: 'Remove Root',
       graph: 'digraph {\n'
           + '\tnode [shape=record];\n'
-          + '\tstruct3 [label=leaf, shape=ellipse];\n'
+          + '\tstruct3 [label=3, shape=ellipse];\n'
           + '\tstruct4 [label="<f-2> |<f-1> 3|<f0> |<f1> 4|<f2> "];\n'
           + '\tstruct4:<f0> -> struct5;\n'
           + '\tstruct4:<f-2> -> struct3;\n'
-          + '\tstruct5 [label=leaf, shape=ellipse];\n'
+          + '\tstruct5 [label=4, shape=ellipse];\n'
           + '\tstruct4:<f2> -> struct6;\n'
-          + '\tstruct6 [label=leaf, shape=ellipse];\n'
+          + '\tstruct6 [label=dummy, shape=ellipse];\n'
           + '}',
     },
   ],
